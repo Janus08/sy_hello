1d640db5048dfa4bc8b9d876ae2fa3df6a5b1de4
home\admin <Janusfous@163.com>
2018-11-12 15:23:18 +0800

    haha

---
+++ scripts/test_search.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+class TestLogin:
+
+    def test_hello1(self):
+        print("hello")
+        assert 1
+
+    def test_hello2(self):
+        print("hello")
+        assert 1
+
